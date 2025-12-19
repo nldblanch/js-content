5b0bc5d3649d89dcd79111c11f44da3a97174566
nathan.blanch <Nathan.Blanch@os.uk>
2025-12-19 09:55:56 +0000

    add test curl request for contributions

---
+++ PR docs/associates.js
@@ -0,0 +1,23 @@
+const associates =  [
+    {
+      name: "Nathan",
+      academy: "February 2025",
+      tip: "I started coming into the office at 8am to get a head start on my day.",
+      github: "nldblanch",
+      contributions: 15
+    },
+    {
+      name: "Merlin",
+      academy: "November 2025",
+      tip: "",
+      github: "",
+      contributions: 3
+    },
+    {
+      name: "Your name here...",
+      academy: "Your cohort here...",
+      tip: "Submit a pull request to add your name!",
+      github: "",
+      contributions: 0
+    }
+  ]
