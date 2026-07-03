d9d1116f500c1bc2554f3a11324f7d5c87f1a52f
Nathan Blanch <nldblanch@gmail.com>
2026-07-03 17:19:55 +0100

    Update Root.tsx

--- webpage/src/pages/RootLayout/Root.tsx
+++ webpage/src/pages/RootLayout/Root.tsx
@@ -1,10 +1,30 @@
-import { Outlet } from 'react-router';
+import { useEffect } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router';
 import { AppFooter } from './components/AppFooter';
 import { AppHeader } from './components/AppHeader';
 import { usePreloadClass } from './hooks/usePreloadClass';
 import { ScrollToTop } from '@src/ui/ScrollToTop';
 const RootLayout = () => {
   usePreloadClass();
+
+  const location = useLocation();
+  const navigate = useNavigate();
+  
+  // one time hook for gh-pages redirects
+  useEffect(() => {
+    if (!location.search.startsWith('?/')) return;
+
+    const raw = location.search.slice(2);
+    const querySeparatorIndex = raw.indexOf('&');
+
+    const rawPath = querySeparatorIndex === -1 ? raw : raw.slice(0, querySeparatorIndex);
+    const rawQuery = querySeparatorIndex === -1 ? '' : raw.slice(querySeparatorIndex + 1);
+
+    const pathname = `/${rawPath.replace(/~and~/g, '&')}`.replace(/\/+/g, '/');
+    const query = rawQuery ? `?${rawQuery.replace(/~and~/g, '&')}` : '';
+
+    navigate(`${pathname}${query}${location.hash}`, { replace: true });
+  }, [location.hash, location.search, navigate]);
 
   return (
     <div className="flex flex-col min-h-screen">
